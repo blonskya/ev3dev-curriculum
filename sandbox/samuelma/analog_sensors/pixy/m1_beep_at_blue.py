82c811b82cf1bae91796e7880acc34bbeb516f2f
samuelma21 <samuelma@rose-hulma.edu>
2017-11-06 13:22:39 -0500

    ready to test pixy

--- sandbox/samuelma/analog_sensors/pixy/m1_beep_at_blue.py
+++ sandbox/samuelma/analog_sensors/pixy/m1_beep_at_blue.py
@@ -32,15 +32,14 @@ def main():
     assert robot.pixy
 
     while not robot.touch_sensor.is_pressed:
-        # TODO: 2. Implement the module as described in the opening comment block.
+        # DONE: 2. Implement the module as described in the opening comment block.
         # It is recommended that you add to your Snatch3r class's constructor the pixy object, as shown
         #   self.pixy = ev3.Sensor(driver_name="pixy-lego")
         #   assert self.pixy
         # Then here you can use a command like width = robot.pixy.value(3)
-
-
-
-
+        width = robot.pixy.value(3)
+        if width > 0:
+            ev3.Sound.beep()
         time.sleep(0.1)
 
     print("Goodbye!")
--- sandbox/samuelma/mqtt/m5_pc_remote_drive.py
+++ sandbox/samuelma/mqtt/m5_pc_remote_drive.py
@@ -22,8 +22,8 @@ You will need to have the following features:
 
 You can start by running the code to see the GUI, but don't expect button clicks to do anything useful yet.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Mary Ashley Samuelson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -32,9 +32,10 @@ import mqtt_remote_method_calls as com
 
 
 def main():
-    # TODO: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
+    # DONE: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
-    mqtt_client = None  # Delete this line, it was added temporarily so that the code we gave you had no errors.
+    mqtt_client = com.MqttClient()
+
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
@@ -63,24 +64,34 @@ def main():
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
     # forward_button and '<Up>' key is done for your here...
-    # forward_button['command'] = lambda: some_callback1(mqtt_client, left_speed_entry, right_speed_entry)
-    # root.bind('<Up>', lambda event: some_callback1(mqtt_client, left_speed_entry, right_speed_entry))
+    forward_button['command'] = lambda: move(mqtt_client, int(left_speed_entry.get()), int(right_speed_entry.get()))
+    root.bind('<Up>', lambda event: move(mqtt_client, int(left_speed_entry.get()), int(right_speed_entry.get())))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
     # left_button and '<Left>' key
+    left_button['command'] = lambda: move(mqtt_client, -int(left_speed_entry.get()), int(right_speed_entry.get()))
+    root.bind('<Left>', lambda event: move(mqtt_client, -int(left_speed_entry.get()), int(right_speed_entry.get())))
 
     stop_button = ttk.Button(main_frame, text="Stop")
     stop_button.grid(row=3, column=1)
     # stop_button and '<space>' key (note, does not need left_speed_entry, right_speed_entry)
+    stop_button['command'] = lambda: stop(mqtt_client)
+    root.bind('<space>', lambda event: stop(mqtt_client))
+
 
     right_button = ttk.Button(main_frame, text="Right")
     right_button.grid(row=3, column=2)
     # right_button and '<Right>' key
+    right_button['command'] = lambda: move(mqtt_client, int(left_speed_entry.get()), -int(right_speed_entry.get()))
+    root.bind('<Right>', lambda event: move(mqtt_client, int(left_speed_entry.get()), -int(right_speed_entry.get())))
 
     back_button = ttk.Button(main_frame, text="Back")
     back_button.grid(row=4, column=1)
     # back_button and '<Down>' key
+    back_button['command'] = lambda: move(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<Down>', lambda event: move(mqtt_client, left_speed_entry, right_speed_entry))
+
 
     up_button = ttk.Button(main_frame, text="Up")
     up_button.grid(row=5, column=0)
@@ -114,6 +125,13 @@ def main():
 # Observations you should make, you did basically this same program using the IR Remote, but your computer can be a
 # remote control that can do A LOT more than an IR Remote.  We are just doing the basics here.
 
+def stop(mqtt_client):
+    print("stop")
+    mqtt_client.send_message("stop")
+
+def move(mqtt_client, rspeed, lspeed):
+    print("move")
+    mqtt_client.send_message("drive_until_otherwise",[rspeed, lspeed])
 
 # Arm command callbacks
 def send_up(mqtt_client):
